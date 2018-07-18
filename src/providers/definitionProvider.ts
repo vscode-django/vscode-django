24f72219cad4afac7d0c3d5b310b0d322bd704c2
Baptiste Darthenay <baptiste@darthenay.fr>
2018-07-18 20:54:15 +0200

    🐛 Fixed the template Go To Definition

--- src/providers/definitionProvider.ts
+++ src/providers/definitionProvider.ts
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 import {
     CancellationToken,
@@ -10,50 +10,54 @@ import {
     TextDocument,
     Uri,
     workspace,
-} from 'vscode';
+} from 'vscode'
 
-const TEMPLATE_PATH_RE = /{% (?:[\w-]+) [\'|\"]([\w/.]*)[\'|\"]/; // {% tag_name "capture/this"
-const TEMPLATE_GLOB = "**/templates/";
-const BEGIN_OF_FILE = new Position(0, 0);
+const TEMPLATE_PATH_RE = /[\'|\"]([\w/\-]+\.[\w]+)[\'|\"]/
+const TEMPLATE_GLOB = "**/templates/"
+const BEGIN_OF_FILE = new Position(0, 0)
 
-let cache: any = {};
+let cache: any = {}
 
 export class TemplatePathProvider implements DefinitionProvider {
 
-    public selector = { scheme: 'file', language: 'django-html' };
+    public selector = { scheme: 'file', language: 'django-html' }
 
     private static getTemplate(document: TextDocument, position: Position, token: CancellationToken): Thenable<Uri | null> {
 
-        let line = document.lineAt(position.line).text;
-        let matches = line.match(TEMPLATE_PATH_RE);
-        if (! matches) return Promise.resolve(null);
+        let line = document.lineAt(position.line).text
+        let matches = line.match(TEMPLATE_PATH_RE)
+        if (! matches) return Promise.resolve(null)
 
-        let path = matches[1];
-        
-        let template_search = `${TEMPLATE_GLOB}${path}`;
-        let exclude = '';
-        let max_results = 1;
+        let path = matches[1]
+        let pos = position.character
+        let cursorOverPath = pos > line.indexOf(path) && pos < line.indexOf(path) + path.length
 
-        if (template_search in cache) return Promise.resolve(cache[template_search])
+        let template_search = `${TEMPLATE_GLOB}${path}`
+        let exclude = ''
+        let max_results = 1
+        let uri
 
-        let uri = workspace.findFiles(template_search, exclude, max_results, token).then(results => {
-            let result = results.length ? results[0] : null
-            if (result) {
-                cache[template_search] = result
-            }
-            return result
-        })
+        if (template_search in cache) {
+            uri = Promise.resolve(cache[template_search])
+        } else {
+            uri = workspace.findFiles(template_search, exclude, max_results, token).then(results => {
+                let result = results.length ? results[0] : null
+                if (result) cache[template_search] = result
+                return result
+            })
+        }
 
-        let pos = position.character;
-        if (pos < line.indexOf(path) || pos > line.indexOf(path) + path.length) return Promise.resolve(null);
-
-        return uri
+        if (cursorOverPath) {
+            return uri
+        } else {
+            return Promise.resolve(null)
+        }
     }
 
     public provideDefinition(document: TextDocument, position: Position, token: CancellationToken): ProviderResult<Definition> {
         return TemplatePathProvider.getTemplate(document, position, token).then(template => {
-            if (! template) return null;
-            return new Location(template, BEGIN_OF_FILE);
+            if (! template) return null
+            return new Location(template, BEGIN_OF_FILE)
         })
     }
 }
