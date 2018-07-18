2e56a70aa355215fc10c500102799e887f548946
Baptiste Darthenay <baptiste@darthenay.fr>
2018-07-18 09:39:59 +0200

    ⚡️ Improved the template Go To Definition

--- src/providers/definitionProvider.ts
+++ src/providers/definitionProvider.ts
@@ -12,26 +12,42 @@ import {
     workspace,
 } from 'vscode';
 
-const TEMPLATE_PATH_RE = /{% (?:include|extends) [\'|\"]([\w/.]*)[\'|\"]/;
-const TEMPLATE_GLOB = "**/templates/**/";
+const TEMPLATE_PATH_RE = /{% (?:[\w-]+) [\'|\"]([\w/.]*)[\'|\"]/; // {% tag_name "capture/this"
+const TEMPLATE_GLOB = "**/templates/";
 const BEGIN_OF_FILE = new Position(0, 0);
 
+let cache: any = {};
 
 export class TemplatePathProvider implements DefinitionProvider {
 
     public selector = { scheme: 'file', language: 'django-html' };
 
     private static getTemplate(document: TextDocument, position: Position, token: CancellationToken): Thenable<Uri | null> {
-        const EXCLUDE = '';
-        const MAX_RESULTS = 1;
-    
-        const matches = document.lineAt(position.line).text.match(TEMPLATE_PATH_RE);
+
+        let line = document.lineAt(position.line).text;
+        let matches = line.match(TEMPLATE_PATH_RE);
         if (! matches) return Promise.resolve(null);
-        const template = TEMPLATE_GLOB + matches[1];
+
+        let path = matches[1];
         
-        return workspace.findFiles(template, EXCLUDE, MAX_RESULTS, token).then(results => {
-            return results.length ? results[0] : null;
+        let template_search = `${TEMPLATE_GLOB}${path}`;
+        let exclude = '';
+        let max_results = 1;
+
+        if (template_search in cache) return Promise.resolve(cache[template_search])
+
+        let uri = workspace.findFiles(template_search, exclude, max_results, token).then(results => {
+            let result = results.length ? results[0] : null
+            if (result) {
+                cache[template_search] = result
+            }
+            return result
         })
+
+        let pos = position.character;
+        if (pos < line.indexOf(path) || pos > line.indexOf(path) + path.length) return Promise.resolve(null);
+
+        return uri
     }
 
     public provideDefinition(document: TextDocument, position: Position, token: CancellationToken): ProviderResult<Definition> {
